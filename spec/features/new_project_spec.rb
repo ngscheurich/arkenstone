dcbe9104d463904055ff93b7fceb4eeaccccc92c
Nicholas Scheurich <nick@scheurich.me>
2016-02-02 17:52:38 -0600

    Remove test for Simple Form install as we use the generator now

--- spec/features/new_project_spec.rb
+++ spec/features/new_project_spec.rb
@@ -13,14 +13,4 @@ RSpec.describe "Generating a new project" do
     expect(File.exist?(ruby_version_file)).to be true
     expect(file_contents(ruby_version_file)).to eq(expected_contents)
   end
-
-  it "installs Simple Form" do
-    simple_form_initializer = File.join(config_path, "/initializers/simple_form.rb")
-    simple_form_locale = File.join(config_path, "/locales/simple_form.en.yml")
-    simple_form_partial = File.join(lib_path, "/templates/html/scaffold/_form.html.erb")
-
-    expect(File.exist?(simple_form_initializer)).to be true
-    expect(File.exist?(simple_form_locale)).to be true
-    expect(File.exist?(simple_form_partial)).to be true
-  end
 end

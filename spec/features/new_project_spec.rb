a5ffdfa40f0a4ecdc75f5883a803f3eabc088946
Nicholas Scheurich <nick@scheurich.me>
2016-02-02 21:52:45 -0600

    Test for initial Git commit

--- spec/features/new_project_spec.rb
+++ spec/features/new_project_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe "Generating a new project" do
     expect(File.exist?(ruby_version_file)).to be true
     expect(file_contents(ruby_version_file)).to eq(expected_contents)
   end
+
+  it "automatically adds all files to local Git repo and makes an initial commit" do
+    git_opts = "--git-dir=#{project_path}/.git"
+    git_opts << " --work-tree=#{project_path}"
+
+    expect(`git #{git_opts} log -1`).to match(/Initial commit/)
+  end
 end
